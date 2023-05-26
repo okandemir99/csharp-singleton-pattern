757fd44a88d975e19df5b8e42bed5660e40e2706
Okan Demir <okand1231@gmail.com>
2023-05-26 14:31:32 +0300

    singleton pattern implementation

--- CSharpSingletonPattern/CSharpSingletonPattern/Program.cs
+++ CSharpSingletonPattern/CSharpSingletonPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 
 namespace CSharpSingletonPattern
 {
@@ -6,7 +7,44 @@ namespace CSharpSingletonPattern
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //artık instanceyi "car" değişkenine atadık
+            Car car=Car.Instance;
+            //car değişkeniyle Car classındaki propertiese erişebiliyoruz
+            car.Marka = "BMW";
+            car.Model = 1999;
+            Console.WriteLine(car.Model);
         }
+    }
+    //Car Sınıfı oluşturuyoruz
+    public class Car
+    {
+        //static ifadeli instance tanımladık (sınıf kurulmadan çalışabilmesi için statik ifadesi kullandım)
+        static Car instance;
+        static Car()
+        {
+            instance = new Car();
+        }
+
+        //Constructor açtık(sınıf yapısını nesne yaptık), returnla istanceyi döndürüyoruz
+        public static Car Instance
+        {
+            get { return instance; }
+        }
+        //
+       
+        //oluşturduğumuz carın özellikleri
+        string marka;
+        int model;
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value; }
+        }
+        public int Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
     }
 }
